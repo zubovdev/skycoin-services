3c9623360d23fec417dc5aedf23ff19ec6ba4ecf
konstantin <zubovdev@gmail.com>
2021-04-17 13:11:05 +0500

    Small changes a HTTP test server and network info.

--- go.mod
+++ go.mod
@@ -3,10 +3,9 @@ module github.com/skycoin/skycoin-services
 go 1.13
 
 require (
-	github.com/aeden/traceroute v0.0.0-20210211061815-03f5f7cb7908 // indirect
-	github.com/ccding/go-stun v0.1.2 // indirect
+	github.com/aeden/traceroute v0.0.0-20210211061815-03f5f7cb7908
+	github.com/gorilla/mux v1.8.0
 	github.com/jaypipes/ghw v0.7.0
-	github.com/shirou/gopsutil v3.21.3+incompatible // indirect
 	github.com/skycoin/skycoin v0.27.1
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0
--- system-survey/cmd/netinfo/netinfo.go
+++ system-survey/cmd/netinfo/netinfo.go
@@ -1,9 +1,7 @@
 package netinfo
 
 import (
-	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 )
@@ -13,8 +11,7 @@ type HttpTest struct {
 }
 
 // Get returns network information.
-// When test != nil, it will run HTTP tests.
-func Get(test *HttpTest) NetworkInfo {
+func Get() NetworkInfo {
 	n := NetworkInfo{}
 	n.IPv4Method, n.IPv6Method = "https://v4.ident.me/", "https://v6.ident.me/"
 	n.IPv4, n.IPv6 = getIP(n.IPv4Method), getIP(n.IPv6Method)
@@ -38,52 +35,16 @@ func Get(test *HttpTest) NetworkInfo {
 		})
 	}
 
-	// Perform HTTP tests.
-	if test != nil {
-		n.HttpTest = test.perform(n.IPv4, n.IPv6)
-	}
-
 	return n
-}
-
-type httpTestResult struct {
-	IPv4Passed bool `json:"ipv4_passed"`
-	IPv6Passed bool `json:"ipv6_passed"`
-}
-
-// perform runs HTTP tests and returns the httpTestResult.
-func (t HttpTest) perform(ipv4, ipv6 string) httpTestResult {
-	http.HandleFunc("/ping", pingHandler)
-
-	// Run HTTP server in goroutine.
-	go func() {
-		err := http.ListenAndServe(fmt.Sprintf(":%v", t.Port), nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	result := httpTestResult{}
-
-	// Perform IPv4 tests.
-	_, err := http.Get(fmt.Sprintf("http://%s:%v/ping", ipv4, t.Port))
-	result.IPv4Passed = err == nil
-
-	// Perform IPv6 tests.
-	_, err = http.Get(fmt.Sprintf("http://[%s]:%v/ping", ipv6, t.Port))
-	result.IPv6Passed = err == nil
-
-	return result
 }
 
 // NetworkInfo information about network.
 type NetworkInfo struct {
-	IPv4       string         `json:"ipv4"`
-	IPv4Method string         `json:"ipv4_method"`
-	IPv6       string         `json:"ipv6"`
-	IPv6Method string         `json:"ipv6_method"`
-	Ifaces     []iface        `json:"ifaces"`
-	HttpTest   httpTestResult `json:"http_test"`
+	IPv4       string  `json:"ipv4"`
+	IPv4Method string  `json:"ipv4_method"`
+	IPv6       string  `json:"ipv6"`
+	IPv6Method string  `json:"ipv6_method"`
+	Ifaces     []iface `json:"ifaces"`
 }
 
 type iface struct {
@@ -103,9 +64,4 @@ func getIP(url string) string {
 	defer res.Body.Close()
 	b, _ := io.ReadAll(res.Body)
 	return string(b)
-}
-
-// pingHandler handles ping request.
-func pingHandler(rw http.ResponseWriter, _ *http.Request) {
-	_, _ = rw.Write([]byte("pong"))
 }

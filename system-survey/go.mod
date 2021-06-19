0d5c9f4514ff77c2be43973624358065801466fc
Konstantin <zubovdev@gmail.com>
2021-06-19 17:15:51 +0500

    More changes

--- system-survey/go.mod
+++ system-survey/go.mod
@@ -1,4 +1,4 @@
-module "system-survey"
+module system-survey
 
 go 1.16
 
@@ -10,7 +10,7 @@ require (
 	github.com/jaypipes/ghw v0.8.0
 	github.com/matishsiao/goInfo v0.0.0-20200404012835-b5f882ee2288
 	github.com/skycoin/dmsg v0.0.0-20210329160412-4e25fc9ad26c
-	github.com/skycoin/skycoin-services v0.0.0-20210518184956-865fd59fcc55 // indirect
+	github.com/skycoin/skycoin v0.27.1 // indirect
 	github.com/stretchr/testify v1.7.0
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
--- system-survey/main.go
+++ system-survey/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/aeden/traceroute"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
@@ -25,6 +27,7 @@ func main() {
 				Usage:       "Check list of installed applications",
 				UsageText:   "apps [command options]",
 				Description: "Reads out the PATH variable and prints all installed applications.",
+				Action:      actionApps,
 				Flags: cli.FlagsByName{
 					jsonFlag,
 					&cli.StringFlag{
@@ -34,35 +37,15 @@ func main() {
 						DefaultText: "All applications",
 					},
 				},
-				Action: func(c *cli.Context) error {
-					apps := cmd.GetAppList(c.String("filter"))
-					if c.Bool("json") {
-						fmt.Println(string(apps.JSON()))
-					} else {
-						fmt.Println(apps.String())
-					}
-					return nil
-				},
 			},
 			&cli.Command{
 				Name:        "golang",
 				Usage:       "Check golang version",
 				UsageText:   "golang [command options]",
 				Description: "Runs the `go version` command and get output as a string or error on failure.",
+				Action:      actionGolang,
 				Flags: cli.FlagsByName{
 					jsonFlag,
-				},
-				Action: func(c *cli.Context) error {
-					gv, err := cmd.GetGolangVersion()
-					if err != nil {
-						return err
-					}
-					if c.Bool("json") {
-						fmt.Println(string(gv.JSON()))
-					} else {
-						fmt.Println(gv.String())
-					}
-					return nil
 				},
 			},
 			&cli.Command{
@@ -70,17 +53,56 @@ func main() {
 				Usage:       "Get information about the host machine",
 				UsageText:   "hwinfo [command options]",
 				Description: "Collects system and hardware information.",
+				Action:      actionHwinfo,
 				Flags: cli.FlagsByName{
 					jsonFlag,
 				},
-				Action: func(c *cli.Context) error {
-					hi := cmd.GetHwinfo()
-					if c.Bool("json") {
-						fmt.Println(string(hi.JSON()))
-					} else {
-						fmt.Println(hi.String())
-					}
-					return nil
+			},
+			&cli.Command{
+				Name:        "traceroute",
+				Usage:       "TraceRoute utility",
+				UsageText:   "traceroute[command options]",
+				Description: "Tracing a route to the host.",
+				Action:      actionTraceroute,
+				Flags: cli.FlagsByName{
+					jsonFlag,
+					&cli.StringFlag{
+						Name:     "dest",
+						Aliases:  []string{"d"},
+						Usage:    "Destination address",
+						Required: true,
+					},
+					&cli.IntFlag{
+						Name:    "port",
+						Aliases: []string{"p"},
+						Usage:   "Destination port",
+						Value:   traceroute.DEFAULT_PORT,
+					},
+					&cli.IntFlag{
+						Name:  "max-hops",
+						Usage: "Maximum hops",
+						Value: traceroute.DEFAULT_MAX_HOPS,
+					},
+					&cli.IntFlag{
+						Name:  "retries",
+						Usage: "Maximum retries for each hop",
+						Value: traceroute.DEFAULT_RETRIES,
+					},
+					&cli.IntFlag{
+						Name:  "timeout",
+						Usage: "Timeout (ms) for each hop",
+						Value: traceroute.DEFAULT_TIMEOUT_MS,
+					},
+					&cli.IntFlag{
+						Name:  "first-hop",
+						Usage: "Start with hop n",
+						Value: traceroute.DEFAULT_FIRST_HOP,
+					},
+					&cli.IntFlag{
+						Name:  "packet-size",
+						Usage: "Size of the sending packet",
+						Value: traceroute.DEFAULT_PACKET_SIZE,
+					},
 				},
 			},
 		},
@@ -88,4 +110,61 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func actionApps(c *cli.Context) error {
+	apps := cmd.GetAppList(c.String("filter"))
+	if c.Bool("json") {
+		fmt.Println(string(apps.JSON()))
+	} else {
+		fmt.Println(apps.String())
+	}
+	return nil
+}
+
+func actionGolang(c *cli.Context) error {
+	gv, err := cmd.GetGolangVersion()
+	if err != nil {
+		return err
+	}
+	if c.Bool("json") {
+		fmt.Println(string(gv.JSON()))
+	} else {
+		fmt.Println(gv.String())
+	}
+	return nil
+}
+
+func actionHwinfo(c *cli.Context) error {
+	hi := cmd.GetHwinfo()
+	if c.Bool("json") {
+		fmt.Println(string(hi.JSON()))
+	} else {
+		fmt.Println(hi.String())
+	}
+	return nil
+}
+
+func actionTraceroute(c *cli.Context) error {
+	result, err := cmd.Traceroute(cmd.TracerouteInput{
+		MaxHops:         c.Int("max-hops"),
+		Retries:         c.Int("retries"),
+		Timeout:         c.Int("timeout"),
+		FirstHop:        c.Int("first-hop"),
+		PacketSize:      c.Int("packet-size"),
+		DestinationIP:   c.String("dest"),
+		DestinationPort: c.Int("port"),
+	})
+	if err != nil {
+		return err
+	}
+
+	if c.Bool("json") {
+		b, _ := json.Marshal(result)
+		fmt.Println(string(b))
+		return nil
+	}
+
+	fmt.Println(result)
+	return nil
 }
